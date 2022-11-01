edd612c32648ea196ed17355f476002b40628967
Xarri Jorge <kodinglife@gmail.com>
2022-11-01 22:45:31 +0000

    Remove link from blog cards

--- components/blogList.vue
+++ components/blogList.vue
@@ -1,7 +1,7 @@
 <template>
     <b-row style="margin-top: 50px" align-h="center">
         <b-card-group v-for="blog of dataList" :key="blog.slug" column>
-            <NuxtLink :to="`blogs/${blog.slug}`">
+            <!-- <NuxtLink :to="`blogs/${blog.slug}`"> -->
                 <b-card style="max-width: 20rem;" class="mb-2 blog-card mx-1" >
                     <b-card-img-lazy :src="blog.image" class="card-img" top />
                     <b-card-title>{{ blog.title }}</b-card-title>
@@ -10,7 +10,7 @@
                     </b-card-text>
                     <NuxtLink :to="`blogs/${blog.slug}`" variant="primary"><b-button variant="primary">Read More</b-button></NuxtLink>
                 </b-card>
-            </NuxtLink>
+            <!-- </NuxtLink> -->
         </b-card-group>
     </b-row>
 </template>
