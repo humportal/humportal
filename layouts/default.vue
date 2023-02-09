591b9f12f1c0d292447cd42ebe5a151ac2ae7b21
Mark Brough <mark.brough@gmail.com>
2023-02-09 17:12:00 +0100

    Reorder navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,11 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item :to="{name: 'index'}">Home</b-nav-item>
-          <b-nav-item :to="{name: 'about'}">About</b-nav-item>
           <b-nav-item :to="{name: 'signatory-progress'}">Signatory Progress</b-nav-item>
           <b-nav-item :to="{name: 'signatory-data'}">Signatory Data</b-nav-item>
           <b-nav-item :to="{name: 'updates'}">Updates</b-nav-item>
           <b-nav-item :to="{name: 'guidance'}">Guidance</b-nav-item>
+          <b-nav-item :to="{name: 'about'}">About</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
