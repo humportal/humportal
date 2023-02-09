1cac9fd1aae287802c563723bb1d4b12097cfdb9
Mark Brough <mark.brough@gmail.com>
2023-02-09 17:09:26 +0100

    Add links to reference docs

--- pages/reference-docs/index.vue
+++ pages/reference-docs/index.vue
@@ -1,10 +1,20 @@
 <template>
+    <b-row style="margin-top: 50px" align-h="center">
+        <b-card-group v-for="doc of documents" :key="doc.slug" column>
+                <b-card style="max-width: 20rem;" class="mb-2 blog-card mx-1" >
+                    <b-card-title class="text-monospace lead font-weight-bold">{{ doc.title }}</b-card-title>
+                    <NuxtLink :to="{name: 'reference-docs-slug', params: { slug: doc.slug}}">
+                        <b-button variant="primary">Read More</b-button>
+                    </NuxtLink>
+                </b-card>
+        </b-card-group>
+    </b-row>
 </template>
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const doc = await $content("documentation").limit(1).fetch()
-    return { doc };
+    const documents = await $content("guidance").fetch()
+    return { documents };
   },
 };
 </script>
