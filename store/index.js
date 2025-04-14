c67c361058284a933dd604b03b10e11cae96c67e
Mark Brough <mark@brough.io>
2025-04-14 16:51:56 +0200

    Correct IATI Dashboard URL

--- store/index.js
+++ store/index.js
@@ -6,7 +6,7 @@ const baseURL = "https://humportal.org";
 const apiURL = "https://humportal.github.io/humportal-data";
 const codelistsApiURL = "https://codelists.codeforiati.org/api/json/en/";
 const analyticsURL =
-  "https://dashboard.iatistandard.org";
+  "https://dashboard.iatistandard.org/stats";
 
 export const state = () => ({
   stats: {
